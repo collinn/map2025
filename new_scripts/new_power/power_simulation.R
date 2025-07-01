3fe96e68eb9041fd8cc96df37b92dbb43c9263ee
sarah_deschamps <deschamps.sias@gmail.com>
2025-07-01 15:56:35 -0500

    Experiment with different sds settings

--- new_scripts/new_power/power_simulation.R
+++ new_scripts/new_power/power_simulation.R
@@ -8,11 +8,19 @@ library(eyetrackSim)
   # bcor = number of cores
   # sigVal = alpha
   # slope = slope of second line
-sds <- data.table(mm = rep(c(F, T, T, T, T), 2),
-                  ar = rep(c(T, T, T, F, F), 2),
-                  bcor = rep(c(T, T, F, T, F), 2),
-                  sigVal = 0.05,
-                  slope = rep(c(0.025, 0.25), each = 5))
+#sds <- data.table(mm = rep(c(F, T, T, T, T), 2),
+#                  ar = rep(c(T, T, T, F, F), 2),
+#                  bcor = rep(c(T, T, F, T, F), 2),
+#                  sigVal = 0.05,
+#                  slope = rep(c(0.025, 0.25), each = 5))
+
+# idx now goes from 1-8, without ar or bcor
+sds <- expand.grid(mm = c(T,F),
+                   # remove ar for now (to run simulations faster)
+                   # ar = c(T,F), 
+                   # remove bcor
+                   sigVal = c(0.025, 0.01),
+                   slope = c(0.025, 0.25))
 
 # Get simulation index from command line arguments
 idx <- as.numeric(commandArgs(TRUE))
@@ -23,14 +31,14 @@ sidx <- sds[idx, ]
 #' Creates fits using generated data and bdots
 #' @param sidx set of parameters
 #' @param nit number of iterations
-createFits <- function(sidx, nit = 25) {
+createFits <- function(sidx, nit = 250) {
 
   slp <- sidx$slope
   ppars <- c(0, slp)
 
   ## only setting one of the times, generate piecewise linear data based on simulation parameters
   dat <- createPlineData(manymeans = sidx$mm,
-                         ar1 = sidx$ar,
+                         ar1 = FALSE, #sidx$ar,
                          distSig = sidx$sigVal,
                          paired = FALSE,
                          pars = ppars,
@@ -43,7 +51,7 @@ createFits <- function(sidx, nit = 25) {
                   time = "time",
                   curveFun = plinePars(),
                   cores = detectCores() - 1L,
-                  cor = sidx$bcor)
+                  cor = FALSE) #sidx$bcor)
 
 
   sm <- bboot(formula = fixations ~ group(A, B),
@@ -55,11 +63,10 @@ createFits <- function(sidx, nit = 25) {
                   permutation = TRUE)$sigTime)
   list(singlemean = sm,
        manymean = mm,
-       permutation = pm,
-       slope = slp)
+       permutation = pm)
 }
 
-N <- 1000 # Number of simulations
+N <- 100 # Number of simulations
 sims <- vector("list", length = N)
 attr(sims, "simsettings") <- sidx
 nn <- paste0("sim", idx)
