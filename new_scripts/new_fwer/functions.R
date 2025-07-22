dfc71e638f9d72fd5b04b778b2f567c4d7814557
sarah_deschamps <deschamps.sias@gmail.com>
2025-07-22 10:33:22 -0500

    Update fwer calculations

--- new_scripts/new_fwer/functions.R
+++ new_scripts/new_fwer/functions.R
@@ -1,8 +1,6 @@
 library(bdots)
 library(eyetrackSim)
 
-#sigs <- readRDS(ff[16])
-
 ## This compute overall TIE across all time points
 getFWER <- function(y) {
   sigs <- readRDS(y)
@@ -10,12 +8,10 @@ getFWER <- function(y) {
   sm <- sapply(sigs, function(x) !is.null(x[[1]])) |> mean()
   mm <- sapply(sigs, function(x) !is.null(x[[2]])) |> mean()
   pm <- sapply(sigs, function(x) !is.null(x[[3]])) |> mean()
-  #pmm <- sapply(sigs, function(x) !is.null(x[[4]])) |> mean()
 
   smt <- sapply(sigs, function(x) timetie(x[[1]])) |> rowMeans()
   mmt <- sapply(sigs, function(x) timetie(x[[2]])) |> rowMeans()
   pmt <- sapply(sigs, function(x) timetie(x[[3]])) |> rowMeans()
-  #pmtm <- sapply(sigs, function(x) timetie(x[[4]])) |> rowMeans()
 
   fwer <- data.table(sm = sm, mm = mm, pm = pm)
   tsmat <- matrix(c(smt, mmt, pmt), byrow = TRUE, nrow = 3,
@@ -31,8 +27,6 @@ timeSliceFwer <- function(y, f = median) {
     apply(z, 1, f)
   })
   y <- Reduce(rbind, y)
-  #y <- sapply(y, rowMeans)
-  #rownames(y) <- paste0("sim", 1:8)
   y
 }
 
@@ -58,63 +52,46 @@ timetie <- function(mm) {
 
 ##########3---------------------------------------------------------
 
-ff <- list.files("~/dissertation/writing/methodology/scripts/argon/40_rds_files", full.names = TRUE)
-
-ff <- list.files("~/dissertation/writing/methodology/scripts/argon/rds_files", full.names = TRUE)
-
-ff <- ff[c(1, 9:16, 2:8)]
+ff <- list.files("~/Desktop/2025-map/map2025/new_scripts/new_fwer/fwer_rds/", full.names = TRUE)
 
 ## Sim settings
-sds <- expand.grid(bdotscor = c(TRUE, FALSE),
-                   ar1 = c(TRUE, FALSE),
-                   manymeans = c(FALSE, TRUE),
-                   paired = c(FALSE, TRUE),
-                   pairedType = 1)
-sds <- as.data.table(sds)
-sds <- sds[!(manymeans == FALSE & paired == TRUE), ]
-dat <- sds[paired == TRUE, ]
-dat1 <- copy(dat)
-dat1$pairedType <- 2
-sds <- rbind(sds, dat1)
-
+sds <- expand.grid(mms = c(T,F), ar = c(T,F), bcor = F)
+bcor_row <- data.frame(mms = F, ar = T, bcor = T)
+sds <- rbind(bcor_row, sds)
 
 gg <- lapply(ff, getFWER)
 fwer <- rbindlist(lapply(gg, `[[`, 1))
 tfwer <- timeSliceFwer(gg, median)
 
-fwer <- as.data.table(cbind(sds, fwer))[order(rev(manymeans), ar1, decreasing = TRUE), ]
-tfwer <- as.data.table(cbind(sds, tfwer))[order(rev(manymeans), ar1, decreasing = TRUE)]
+fwer <- as.data.table(cbind(sds, fwer))[order(rev(mms), ar, decreasing = TRUE), ]
+tfwer <- as.data.table(cbind(sds, tfwer))[order(rev(mms), ar, decreasing = TRUE)]
 
-fwer <- fwer[order(manymeans), ]
-tfwer <- tfwer[order(manymeans), ]
+fwer <- fwer[order(mms), ]
+tfwer <- tfwer[order(mms), ]
 
-fwer[, `:=`(bdotscor = ifelse(bdotscor, "Yes", "No"),
-            ar1 = ifelse(ar1, "Yes", "No"),
-            manymeans = ifelse(manymeans, "Yes", "No"),
-            paired = ifelse(paired, "Yes", "No"))]
+fwer[, `:=`(bcor = ifelse(bcor, "Yes", "No"),
+            ar = ifelse(ar, "Yes", "No"),
+            mms = ifelse(mms, "Yes", "No"))]
 
-tfwer[, `:=`(bdotscor = ifelse(bdotscor, "Yes", "No"),
-            ar1 = ifelse(ar1, "Yes", "No"),
-            manymeans = ifelse(manymeans, "Yes", "No"),
-            paired = ifelse(paired, "Yes", "No"))]
+tfwer[, `:=`(bcor = ifelse(bcor, "Yes", "No"),
+            ar = ifelse(ar, "Yes", "No"),
+            mms = ifelse(mms, "Yes", "No"))]
 
-fwer <- fwer[, .(manymeans, ar1, bdotscor, paired, pairedType, sm, mm, pm)]
-tfwer <- tfwer[, .(manymeans, ar1, bdotscor, paired, pairedType, sm, mm, pm)]
+fwer <- fwer[, .(mms, ar, bcor, sm, mm, pm)]
+tfwer <- tfwer[, .(mms, ar, bcor, sm, mm, pm)]
 
-fwers <- split(fwer, by = c("paired", "pairedType"))
-tfwers <- split(tfwer, by = c("paired", "pairedType"))
 
 # rev(paired)
 library(xtable)
 ## unpaired
-fwers[[1]][, c(1:3,6:8)] |> xtable() |> print(include.rownames = FALSE)
+#fwers[[1]][, c(1:3,6:8)] |> xtable() |> print(include.rownames = FALSE)
 # Paired #1
-fwers[[2]][, c(1:3,6:8)] |> xtable() |> print(include.rownames = FALSE)
-fwers[[3]][, c(1:3,6:8)] |> xtable() |> print(include.rownames = FALSE)
+#fwers[[2]][, c(1:3,6:8)] |> xtable() |> print(include.rownames = FALSE)
+#fwers[[3]][, c(1:3,6:8)] |> xtable() |> print(include.rownames = FALSE)
 
-tfwers[[1]][, c(1:3,6:8)] |> xtable() |> print(include.rownames = FALSE)
-tfwers[[2]][, c(1:3,6:8)] |> xtable() |> print(include.rownames = FALSE)
-tfwers[[3]][, c(1:3,6:8)] |> xtable() |> print(include.rownames = FALSE)
+#tfwers[[1]][, c(1:3,6:8)] |> xtable() |> print(include.rownames = FALSE)
+#tfwers[[2]][, c(1:3,6:8)] |> xtable() |> print(include.rownames = FALSE)
+#tfwers[[3]][, c(1:3,6:8)] |> xtable() |> print(include.rownames = FALSE)
 
-twfer |> xtable() |> print(include.rownames = FALSE)
+#twfer |> xtable() |> print(include.rownames = FALSE)
 
