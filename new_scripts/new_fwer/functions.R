91b5ab94d4af3d754b530e24a2df038dbebec145
yantiffa <yantiffa@grinnell.edu>
2025-07-24 09:54:54 -0500

    Update functions.R - documentation added

--- new_scripts/new_fwer/functions.R
+++ new_scripts/new_fwer/functions.R
@@ -1,17 +1,20 @@
+# Load required R packages
 library(bdots)
 library(eyetrackSim)
 
 ## This compute overall TIE across all time points
 getFWER <- function(y) {
-  sigs <- readRDS(y)
+  sigs <- readRDS(y) # Load simulation results from RDS file
+  # Compute overall FWER: proportion of sims with any significant timepoint
 
-  sm <- sapply(sigs, function(x) !is.null(x[[1]])) |> mean()
-  mm <- sapply(sigs, function(x) !is.null(x[[2]])) |> mean()
-  pm <- sapply(sigs, function(x) !is.null(x[[3]])) |> mean()
-
-  smt <- sapply(sigs, function(x) timetie(x[[1]])) |> rowMeans()
-  mmt <- sapply(sigs, function(x) timetie(x[[2]])) |> rowMeans()
-  pmt <- sapply(sigs, function(x) timetie(x[[3]])) |> rowMeans()
+  sm <- sapply(sigs, function(x) !is.null(x[[1]])) |> mean() # homogeneous bootstrap method
+  mm <- sapply(sigs, function(x) !is.null(x[[2]])) |> mean() # Heterogeneous bootstrap method
+  pm <- sapply(sigs, function(x) !is.null(x[[3]])) |> mean() # Permutation method
+               
+  # Compute per-timepoint error rates across simulations
+  smt <- sapply(sigs, function(x) timetie(x[[1]])) |> rowMeans() # homogeneous bootstrap per-timepoint error rates
+  mmt <- sapply(sigs, function(x) timetie(x[[2]])) |> rowMeans() # Heterogeneous bootstrap per-timepoint error rates
+  pmt <- sapply(sigs, function(x) timetie(x[[3]])) |> rowMeans() # Permutation per-timepoint error rates
 
   fwer <- data.table(sm = sm, mm = mm, pm = pm)
   tsmat <- matrix(c(smt, mmt, pmt), byrow = TRUE, nrow = 3,
@@ -22,9 +25,9 @@ getFWER <- function(y) {
 ## This takes the list returned by getfwer
 timeSliceFwer <- function(y, f = median) {
   nn <- length(y)
-  y <- lapply(y, `[[`, 2)
+  y <- lapply(y, `[[`, 2) # Extract time-slice matrices
   y <- lapply(y, function(z) {
-    apply(z, 1, f)
+    apply(z, 1, f) # Apply summary function across timepoints
   })
   y <- Reduce(rbind, y)
   y
@@ -36,11 +39,11 @@ timetie <- function(mm) {
   vec <- vector("numeric", length = length(time))
 
   if (is.null(dim(mm))) {
-    vec <- as.logical(vec)
+    vec <- as.logical(vec) # Default: return all FALSE if no significance
     return(vec)
   }
 
-  # each of these into a list
+  # Convert significance windows to individual timepoints
   sm <- split(mm, row(mm))
   bv <- lapply(sm, function(m) {
     sigt <- do.call(seq, as.list(c(m, 4)))
@@ -51,14 +54,17 @@ timetie <- function(mm) {
 }
 
 ##########3---------------------------------------------------------
-
+# Load simulation output files
 ff <- list.files("~/Desktop/2025-map/map2025/new_scripts/new_fwer/fwer_rds/", full.names = TRUE)
 
-## Sim settings
-sds <- expand.grid(mms = c(T,F), ar = c(T,F), bcor = F)
-bcor_row <- data.frame(mms = F, ar = T, bcor = T)
+## Sim settings - recreate parameter grid from simulation
+sds <- expand.grid(mm = c(T,F), # many-means/single-means parameter
+                   ar = c(T,F), # AR(1) parameter
+                   bcor = F) # Bootstrap correlation
+bcor_row <- data.frame(mms = F, ar = T, bcor = T) # Additional parameter combo for bcor = T
 sds <- rbind(bcor_row, sds)
 
+# Process all simulation files to compute FWER
 gg <- lapply(ff, getFWER)
 fwer <- rbindlist(lapply(gg, `[[`, 1))
 tfwer <- timeSliceFwer(gg, median)
@@ -66,9 +72,11 @@ tfwer <- timeSliceFwer(gg, median)
 fwer <- as.data.table(cbind(sds, fwer))[order(rev(mms), ar, decreasing = TRUE), ]
 tfwer <- as.data.table(cbind(sds, tfwer))[order(rev(mms), ar, decreasing = TRUE)]
 
+# Re-sort by mms for order
 fwer <- fwer[order(mms), ]
 tfwer <- tfwer[order(mms), ]
 
+# Convert logical parameters to readable labels for tables
 fwer[, `:=`(bcor = ifelse(bcor, "Yes", "No"),
             ar = ifelse(ar, "Yes", "No"),
             mms = ifelse(mms, "Yes", "No"))]
@@ -80,7 +88,7 @@ tfwer[, `:=`(bcor = ifelse(bcor, "Yes", "No"),
 fwer <- fwer[, .(mms, ar, bcor, sm, mm, pm)]
 tfwer <- tfwer[, .(mms, ar, bcor, sm, mm, pm)]
 
-
+## For LaTeX table generation
 # rev(paired)
 library(xtable)
 ## unpaired
